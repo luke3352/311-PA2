817f3317bd1c1989a07d68d7e13fddde5561c96d
luke3352 <luke33@iastate.edu>
2017-11-06 20:36:25 -0600

    Initial Commit

---
+++ WikiCrawler.java
@@ -0,0 +1,37 @@
+// LEAVE THIS FILE IN THE DEFAULT PACKAGE
+//  (i.e., DO NOT add 'package cs311.pa1;' or similar)
+
+// DO NOT MODIFY THE EXISTING METHOD SIGNATURES
+//  (you may, however, add additional methods and fields)
+
+// DO NOT INCLUDE LIBRARIES OUTSIDE OF THE JAVA STANDARD LIBRARY
+//  (i.e., you may include java.util.ArrayList etc. here, but not junit, apache commons, google guava, etc.)
+
+import java.util.ArrayList;
+
+
+public class WikiCrawler
+{
+    static final String BASE_URL = "https://en.wikipedia.org";
+    // other member fields and methods
+
+    public WikiCrawler(String seedUrl, int max, ArrayList<String> topics, String fileName)
+    {
+        // implementation
+    }
+
+    // NOTE: extractLinks takes the source HTML code, NOT a URL
+    public ArrayList<String> extractLinks(String doc)
+    {
+        // implementation
+        return null;
+    }
+
+    public void crawl()
+    {
+        // implementation
+    }
+}
+
+
+
