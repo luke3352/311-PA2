12d22e07d40059e8a1dc241dbfd7031f702ff5c5
Nischay Venkatram <nischay.v@gmail.com>
2017-11-15 22:05:32 -0600

    remove comments

--- src/WikiCrawler.java
+++ src/WikiCrawler.java
@@ -34,23 +34,6 @@ public class WikiCrawler
         this.seedUrl = seedUrl;
     }
 
-    // NOTE: extractLinks takes the source HTML code, NOT a URL
-    /*
-    extractLinks(String doc). This method gets a string (that represents contents of a .html
-    file) as parameter. This method should return an array list (of Strings) consisting of links from doc.
-    Type of this method is ArrayList<String>. You may assume that the html page is the source
-    (html) code of a wiki page. This method must
-• Extract only wiki links. I.e. only links that are of form /wiki/XXXXX.
-• Only extract links that appear after the first occurrence of the html tag <p> (or <P>).
-• Should not extract any wiki link that contain the characters “#” or “:”.
-• The order in which the links in the returned array list must be exactly the same order in
-which they appear in doc.
-For example, if doc looks like the attached file (sample.txt), then
-Then the returned list must be
-/wiki/Science, /wiki/Computation, /wiki/Procedure_(computer_science),
-/wiki/Algorithm, /wiki/Information, /wiki/CiteSeerX, /wiki/Charles_Babbage
-
-     */
     public ArrayList<String> extractLinks(String doc)
     {
 
@@ -121,29 +104,6 @@ Then the returned list must be
 
         return matches;
     }
-    /*
-
-    crawl() This method should construct the web graph over following pages: If seedUrl does
-not contain all words from topics, then the graph constructed is empty graph(0 vertices and 0
-edges). Suppose that seedUrl contains all words from topics. Consider the first max many pages
-(including seedUrl page), that contain every word from the list topics, that are visited when you
-do a BFS with seedUrl as root. Your program should construct the web graph only over those
-pages. and writes the graph to the file fileName.
-
-For example, WikiCrawler can be used in a program as follows. Say topics has strings Iowa
-State, Cyclones.
-WikiCrawler w = new WikiCrawler("/wiki/Iowa_State_University", 100, topics, "WikiISU.txt");
-w.crawl();
-
-This program will start crawling with /wiki/Iowa State University as the root page. It will
-collect the first 100 pages that contain both the words “Iowa State” and ”Cyclones”
-that are visited by a BFS algorithm. Determines the graph (links among the those 100 pages)
-and writes the graph to a (text) file named WikiISU.txt. This file will contain all edges of the
-graph. Each line of this file should have one directed edge, except the first line. The first line of
-the graph should indicate number of vertices which will be 100. Below is sample contents of the
-file
-
-     */
 
     public void crawl()
     {
